b2d1ef2876abc650e731a158efdd295f27b3d4c7
Nuno Lopes <nlo@boozt.com>
2016-07-16 14:57:07 +0200

    Add logo with better resolution

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ var AlfredNode = require('alfred-workflow-nodejs');
 var request = require('request');
 var actionHandler = AlfredNode.actionHandler;
 var workflow = AlfredNode.workflow;
-
-workflow.setName("imdb");
+workflow.setName('imdb');
 
 (function main()
 {
-    actionHandler.onAction("search", function(query)
+    actionHandler.onAction('search', function(query)
     {
         var options = {
             url: 'http://www.omdbapi.com',
@@ -24,20 +23,32 @@ workflow.setName("imdb");
             if (!error && response.statusCode == 200)
             {
                 var result = JSON.parse(body);
-                var subtitle = [
-                    result['Runtime'],
-                    "Metascore: " + result['Metascore'],
-                    "Rating: " + result['imdbRating']
-                ];
-
                 var Item = AlfredNode.Item;
-                var item = new Item({
-                    title: result['Title'] + " (" + result['Year'] + ")",
-                    subtitle: subtitle.join(" | "),
-                    arg: result['imdbID'],
-                    icon: "link.png",
-                    valid: true
-                });
+                var item = Object;
+
+                if (result['Response'] == 'True')
+                {
+                    var subtitle = [
+                        result['Runtime'],
+                        'Metascore: ' + result['Metascore'],
+                        'Rating: ' + result['imdbRating']
+                    ];
+
+                    item = new Item({
+                        title: result['Title'] + ' (' + result['Year'] + ')',
+                        subtitle: subtitle.join(' | '),
+                        arg: result['imdbID'],
+                        icon: 'link.png',
+                        valid: true
+                    });
+                }
+                else
+                {
+                    item = new Item({
+                        title: '"' + query + '" sleep with the fishes...',
+                        valid: true
+                    });
+                }
 
                 workflow.addItem(item);
                 workflow.feedback();
